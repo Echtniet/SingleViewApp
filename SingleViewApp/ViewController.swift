41a4a33fe461f1e614abe65b6795e4c3a2b62988
clinton davelaar <davelaarprogramming@gmail.com>
2019-09-06 12:11:36 -0500

    Initial Commit

---
+++ SingleViewApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by Davelaar,Clinton B on 9/6/19.
+//  Copyright © 2019 Davelaar,Clinton B. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
